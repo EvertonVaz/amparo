c93fe941cbd9e9bda0a4a0e23c7f58bd76409dc1
Everton <egeraldo@student.42sp.org.br>
2024-10-09 21:01:53 -0300

    refactor: removendo comentarios desnecessarios

--- main.go
+++ main.go
@@ -8,11 +8,9 @@ import (
 func main() {
 	router := gin.Default()
 
-	// Define os endpoints da API
 	router.POST("/important-dates/:userId", handlers.CreateImportantDates)
 	router.GET("/important-dates/:userId", handlers.GetImportantDates)
 	router.DELETE("/important-dates/:userId", handlers.DeleteImportantDates)
 
-	// Inicia o servidor na porta 8080
 	router.Run(":8080")
 }
